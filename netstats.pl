da91b84c19e42fa90cd9d31bc5cac8a0bc196e31
spek <spek@localhost>
2015-09-04 08:47:34 -0600

    Cleaned up/enhanced help output, created help() sub

--- netstats.pl
+++ netstats.pl
@@ -11,30 +11,27 @@ use warnings;
 use Getopt::Long;
 my @stat = split '\n', `netstat -nat`;
 
-
 my %statuses = map { $_=> undef } qw( ESTABLISHED  SYN_SENT SYN_RECV FIN_WAIT1
                                   FIN_WAIT2 TIME_WAIT CLOSE CLOSE_WAIT
                                   LAST_ACK LISTEN CLOSING UNKNOWN );
 my $given_args = scalar @ARGV;
 
 unless ( GetOptions (
-                        "ESTABLISHED" => \$statuses{ESTABLISHED},
+                        "ESTABLISHED|E" => \$statuses{ESTABLISHED},
                         "SYN_SENT|SS" => \$statuses{SYN_SENT},
                         "SYN_RECV|SR" => \$statuses{SYN_RECV},
                         "FIN_WAIT1|F1" => \$statuses{FIN_WAIT1},
                         "FIN_WAIT2|F2" => \$statuses{FIN_WAIT2},
-                        "TIME_WAIT" => \$statuses{TIME_WAIT},
+                        "TIME_WAIT|TW" => \$statuses{TIME_WAIT},
                         "CLOSE|C" => \$statuses{CLOSE},
                         "CLOSE_WAIT|CW" => \$statuses{CLOSE_WAIT},
-                        "LAST_ACK" => \$statuses{LAST_ACK},
-                        "LISTEN" => \$statuses{LISTEN},
-                        "CLOSING" => \$statuses{CLOSING},
-                        "UNKNOWN" => \$statuses{UNKNOWN},
-
+                        "LAST_ACK|LA" => \$statuses{LAST_ACK},
+                        "LISTEN|L" => \$statuses{LISTEN},
+                        "CLOSING|CG" => \$statuses{CLOSING},
+                        "UNKNOWN|U" => \$statuses{UNKNOWN},
+                        "help" => \&help,
                         )) {
-                            print "USAGE $0:\n";
-                            print while <DATA>;
-                            exit 1;
+                            help();
                         }
 if ($given_args == 0){map {$statuses{$_}=1} keys  %statuses}
 
@@ -57,16 +54,40 @@ for (@stat){
     $data{$status}++ if defined $data{$status};
 }
 
-map { printf "%10s\t$data{$_}\n ",$_} sort grep {defined $statuses{$_}} keys %statuses;
+map { printf "%10s\t$data{$_}\n ",$_} 
+    sort grep {defined $statuses{$_}} 
+    keys %statuses;
+
+sub help {
+    print "USAGE $0:\n";
+    print while <DATA>;
+    exit 1;
+}
 
 __DATA__
-OPTIONS:
-Options specifies which status will be reported in the output.
-Name of status can be given in upper or lower case and in abbreviated form as
-Getopt::Long is used. Additionally you can use SS insetead of SYN_SENT, SR for
-SYN_RECV, F1 for FIN_WAIT1, F2 for FIN_WAIT2, C for CLOSE and CW for CLOSE_WAIT.
 
-If no options are given all status will be printed.
+OPTIONS:
+
+Options specifies which status will be reported in the output.
+Name of status can be given in upper or lower case.
+
+If no options are given all statuses will be printed.
+
+You can use the following option abbreviations:
+
+-E  for --ESTABLISHED
+-SS for --SYN_SENT
+-SR for --SYN_RECV
+-F1 for --FIN_WAIT1 
+-F2 for --FIN_WAIT2 
+-TW for --Time_WAIT
+-C  for --CLOSE
+-CW for --CLOSE_WAIT
+-LA for --LAST_ACK
+-L  for --LISTEN
+-CG for --CLOSING
+-U  for --UNKNOWN
+-h  for --help
 
 Here a brief description of status meanings:
 
